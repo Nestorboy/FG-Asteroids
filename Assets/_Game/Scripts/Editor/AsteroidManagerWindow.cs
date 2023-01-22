74ce62fc319cc37d63e6d1b90675e3d729dce7b3
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-01-22 15:03:33 +0100

    Finished AsteroidManagerWindow.
    • Finished implementing the AsteroidManagerWindow.
    • Removed unused CustomEditorWindow.uss file.
    • Updated AsteroidManagerWindow layout.

--- Assets/_Game/Scripts/Editor/AsteroidManagerWindow.cs
+++ Assets/_Game/Scripts/Editor/AsteroidManagerWindow.cs
@@ -1,35 +1,124 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
 
 public class AsteroidManagerWindow : EditorWindow
 {
+    private VisualElement _root;
+    [SerializeField] private bool _initialized;
+    
+    [SerializeField] private Asteroids.AsteroidSpawner _spawner;
+    private Box _spawnerContainer;
+    private ObjectField _spawnerField;
+    private InspectorElement _spawnerInspector;
+
+    [SerializeField] private Ship.Engine _engine;
+    private Box _engineContainer;
+    private ObjectField _engineField;
+    private InspectorElement _engineInspector;
+    
+    private Asteroids.AsteroidSpawner spawner
+    {
+        get => _spawner;
+        set
+        {
+            _spawner = value;
+            
+            _spawnerContainer.Clear();
+
+            _spawnerInspector = new InspectorElement(value);
+            if (value == null)
+            {
+                _spawnerContainer.Add(new Label("Please select an AsteroidSpawner to modify."));
+            }
+            else
+            {
+                _spawnerContainer.Add(_spawnerInspector);
+            }
+        }
+    }
+    
+    private Ship.Engine engine
+    {
+        get => _engine;
+        set
+        {
+            _engine = value;
+            
+            _engineContainer.Clear();
+
+            _engineInspector = new InspectorElement(value);
+            if (value == null)
+            {
+                _engineContainer.Add(new Label("Please select an Engine to modify."));
+            }
+            else
+            {
+                _engineContainer.Add(_engineInspector);
+            }
+        }
+    }
+
     [MenuItem("Window/Nessie/Asteroid Manager")]
     public static void OpenWindow()
     {
-        GetWindow<AsteroidManagerWindow>("Asteroid Manager");
+        var window = GetWindow<AsteroidManagerWindow>("Asteroid Manager");
+        window.minSize = new Vector2(350, 350);
+    }
+
+    public void OnEnable()
+    {
+        Undo.undoRedoPerformed += UndoCallback;
+        
+        if (_initialized)
+            return;
+        
+        _spawner = FindObjectOfType<Asteroids.AsteroidSpawner>();
+        _engine = FindObjectOfType<Ship.Engine>();
+
+        _initialized = true;
     }
 
     public void CreateGUI()
     {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
+        _root = rootVisualElement;
+        
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/_Game/Scripts/Editor/CustomEditorWindow.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
+        VisualElement fromUXML = visualTree.Instantiate();
+        _root.Add(fromUXML);
+        
+        _spawnerContainer = new Box();
+        _spawnerField = fromUXML.Q<ObjectField>("SpawnerField");
+        _spawnerField.parent.Add(_spawnerContainer);
+        _spawnerField.value = _spawner;
+        spawner = _spawner;
+        _spawnerField.RegisterValueChangedCallback(SpawnerChanged);
+        
+        _engineContainer = new Box();
+        _engineField = fromUXML.Q<ObjectField>("EngineField");
+        _engineField.parent.Add(_engineContainer);
+        _engineField.value = _engine;
+        engine = _engine;
+        _engineField.RegisterValueChangedCallback(ShipChanged);
+    }
 
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/_Game/Scripts/Editor/CustomEditorWindow.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+    private void UndoCallback()
+    {
+        _spawnerField.value = spawner;
+        _engineField.value = engine;
+    }
+    
+    private void SpawnerChanged(ChangeEvent<Object> evt)
+    {
+        Undo.RecordObject(this, "Changed Spawner");
+        spawner = (Asteroids.AsteroidSpawner)evt.newValue;
+    }
+
+    private void ShipChanged(ChangeEvent<Object> evt)
+    {
+        Undo.RecordObject(this, "Changed Engine");
+        engine = (Ship.Engine)evt.newValue;
     }
 }
